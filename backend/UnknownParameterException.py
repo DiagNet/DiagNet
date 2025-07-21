291db8feff2b4443ec81ef242b7319894e8341b1
DrPacar <lukapacar11@gmail.com>
2025-07-21 15:42:31 +0200

    backend: add Exceptions for handling tests.

---
+++ backend/UnknownParameterException.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Author: Luka Pacar
+Date: 2025-07-21
+Description: UnknownParameterException
+"""
+__author__ = "Luka Pacar"
+__version__ = "1.0.0"
+
+class UnknownParameterException(Exception):
+    """Exception raised when an unexpected Parameter is parsed."""
+    pass
