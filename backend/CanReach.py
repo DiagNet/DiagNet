ac6d6716a513140089850c1863df6dcc510b3e2b
Benedikt <benedikt.theuretzbachner@gmail.com>
2025-08-08 16:38:03 +0200

    backend: refactor CanReach.py

--- backend/CanReach.py
+++ backend/CanReach.py
@@ -5,7 +5,7 @@ from genie.testbed import load
 class CanReach(Test):
     _required_params = ["device", "ping_host"]
 
-    def test_reachability(self):
+    def test_reachability(self) -> bool:
         response: dict = self.device.parse(f"ping {self.ping_host}")
         success_rate: float = response["ping"]["statistics"]["success_rate_percent"]
 
@@ -15,7 +15,7 @@ class CanReach(Test):
 if __name__ == "__main__":
     inventory = load("testbed.yaml")
     device = inventory.devices["R1"]
-    device.connect(log_stdout=False, log_file="/dev/null")
+    device.connect(log_stdout=False)
 
     canreach = CanReach()
     print(canreach.run(device=device, ping_host="10.0.0.2"))
