2725047f4bd088ddbb2093331d7efc4a8b368555
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-08-08 16:42:19 +0200

    dashboard: init

--- dn_site/urls.py
+++ dn_site/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from dashboard import views as dashboard_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", dashboard_views.index, name="dashboard"),
 ]
