87270731d781689497236622d2a98a27c455b8b8
Benedikt <benedikt.theuretzbachner@gmail.com>
2025-08-08 16:17:54 +0200

    backend: add testcase for pinging an ip (CanReach.py)

---
+++ backend/CanReach.py
@@ -0,0 +1,21 @@
+from Test import Test
+from genie.testbed import load
+
+
+class CanReach(Test):
+    _required_params = ["device", "ping_host"]
+
+    def test_reachability(self):
+        response: dict = self.device.parse(f"ping {self.ping_host}")
+        success_rate: float = response["ping"]["statistics"]["success_rate_percent"]
+
+        return success_rate >= 60.0
+
+
+if __name__ == "__main__":
+    inventory = load("testbed.yaml")
+    device = inventory.devices["R1"]
+    device.connect(log_stdout=False, log_file="/dev/null")
+
+    canreach = CanReach()
+    print(canreach.run(device=device, ping_host="10.0.0.2"))
