a1880b4882576ca15a520bc3065057ecb85b5d16
Benedikt <benedikt.theuretzbachner@gmail.com>
2025-08-12 16:26:05 +0200

    backend: add testcase if ip is not reachable (CannotPing.py)

---
+++ backend/CannotReach.py
@@ -0,0 +1,21 @@
+from Test import Test
+from genie.testbed import load
+
+
+class CannotReach(Test):
+    _required_params = ["device", "ping_host"]
+
+    def test_reachability(self) -> bool:
+        response: dict = self.device.parse(f"ping {self.ping_host}")
+        success_rate: float = response["ping"]["statistics"]["success_rate_percent"]
+
+        return success_rate == 0.0
+
+
+if __name__ == "__main__":
+    inventory = load("testbed.yaml")
+    device = inventory.devices["R1"]
+    device.connect(log_stdout=False)
+
+    tester = CannotReach()
+    print(tester.run(device=device, ping_host="10.0.0.2"))
