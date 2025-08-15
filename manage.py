83df85ab92e69a448cfb848c60b766dd7e5429fd
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-08-15 12:56:39 +0200

    treewide: rename dn_site to diagnet

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dn_site.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "diagnet.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
