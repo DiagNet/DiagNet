9b5383fd2a077146ee9d76fe1c5db586391aa4d2
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-08-15 12:56:39 +0200

    treewide: rename dn_site to diagnet

--- diagnet/asgi.py
+++ diagnet/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for dn_site project.
+ASGI config for diagnet project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dn_site.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "diagnet.settings")
 
 application = get_asgi_application()
--- diagnet/wsgi.py
+++ diagnet/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for dn_site project.
+WSGI config for diagnet project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dn_site.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "diagnet.settings")
 
 application = get_wsgi_application()
