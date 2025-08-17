972ea58e8a64b27c797b4c6b4b48a4c669b26581
DrPacar <lukapacar11@gmail.com>
2025-08-17 17:08:19 +0200

    networktests/models: add TestCase

--- networktests/models.py
+++ networktests/models.py
@@ -1,1 +1,10 @@
-# Create your models here.
+from django.db import models
+
+class TestCase(models.Model):
+    test_module = models.TextField()
+    expected_result = models.BooleanField()
+    label = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.test_module} - {self.label}"
