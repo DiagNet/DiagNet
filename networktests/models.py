a086a9207cfe1026a1935b706dd893b41d6d9f67
DrPacar <lukapacar11@gmail.com>
2025-08-17 17:11:06 +0200

    networktests/models: change TestCase string method

--- networktests/models.py
+++ networktests/models.py
@@ -7,4 +7,4 @@ class TestCase(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.test_module} - {self.label}"
+        return f"{self.label}"
