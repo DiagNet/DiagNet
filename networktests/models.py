8eea7a1350d08c50fa9a16204db817229781da33
DrPacar <lukapacar11@gmail.com>
2025-08-17 17:16:18 +0200

    networktests/models: add TestParameter

--- networktests/models.py
+++ networktests/models.py
@@ -8,3 +8,13 @@ class TestCase(models.Model):
 
     def __str__(self):
         return f"{self.label}"
+
+class TestParameter(models.Model):
+    name = models.TextField()
+    # All Parameters accessible using test_case.parameters.all()
+    # When the parent TestCase is deleted the Parameter is deleted as well.
+    test_case = models.ForeignKey(TestCase, related_name='parameters', on_delete=models.CASCADE)
+    value = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name}: {self.value}"
