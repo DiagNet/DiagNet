0b88c7b52e901a933f1e74efc772e4b608cfa079
DrPacar <lukapacar11@gmail.com>
2025-09-12 16:11:55 +0200

    networktests: create popup for creating tests + API calls

--- networktests/urls.py
+++ networktests/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path("get-all-testcases", views.get_all_testcases, name="get-all-testcases"),
+    path("api/search/test/classes", views.search_all_available_testcases, name="search_testclasses_api"),
+    path("api/search/test/parameters", views.get_parameters_of_specific_testcase, name="search_test_parameters_api"),
+    path("api/create/test", views.create_test, name="create_test_api"),
     path("test/", views.test_page, name="test-page")
 ]
