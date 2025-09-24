ecd63271925a02e9782a6ac6773479153a6bfb44
Benedikt <benedikt.theuretzbachner@gmail.com>
2025-09-24 19:56:43 +0200

    DN-27 testgroups: add delete action

--- testgroups/urls.py
+++ testgroups/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.testgroups_page, name="testgroups-page"),
-    path("list-testgroups", views.list_testgroups, name="list-testgroups"),
+    path("list-testgroups/", views.list_testgroups, name="list-testgroups"),
     path("create-testgroup/", views.create_testgroup, name="create-testgroup"),
+    path("delete-testgroup/", views.delete_testgroup, name="delete-testgroup"),
 ]
