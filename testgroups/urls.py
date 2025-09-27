9b791838168aece3926dc171d952f231acd4436f
Benedikt <benedikt.theuretzbachner@gmail.com>
2025-09-27 17:50:46 +0200

    DN-27 feat/testgroups: add page for testgroup detail

--- testgroups/urls.py
+++ testgroups/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("list-testgroups/", views.list_testgroups, name="list-testgroups"),
     path("create-testgroup/", views.create_testgroup, name="create-testgroup"),
     path("delete-testgroup/", views.delete_testgroup, name="delete-testgroup"),
+    path("detail/<str:name>/", views.get_testgroup_detail, name="testgroup-detail"),
+    path("list-testcases/<str:name>/", views.list_testcases, name="list-testcases"),
 ]
