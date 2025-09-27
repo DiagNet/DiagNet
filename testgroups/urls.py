2c4c92d1108b79a859ae455e9aa780983be16cce
Benedikt <benedikt.theuretzbachner@gmail.com>
2025-09-27 19:27:45 +0200

    DN-27 feat/testgroups: make testgroups renamable (once)

--- testgroups/urls.py
+++ testgroups/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("delete-testgroup/", views.delete_testgroup, name="delete-testgroup"),
     path("detail/<str:name>/", views.get_testgroup_detail, name="testgroup-detail"),
     path("list-testcases/<str:name>/", views.list_testcases, name="list-testcases"),
+    path("rename/<str:name>/", views.rename_testgroup, name="rename-testgroup"),
 ]
