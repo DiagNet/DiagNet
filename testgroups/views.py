3f23dadfdf19a9a3eb6cf7707e4dfa8861884d16
Benedikt <benedikt.theuretzbachner@gmail.com>
2025-09-27 19:27:45 +0200

    DN-27 feat/testgroups: make testgroups renamable (once)

--- testgroups/views.py
+++ testgroups/views.py
@@ -33,7 +33,7 @@ def create_testgroup(request):
     if request.method != "POST":
         return HttpResponse("bad request method.")
 
-    name = name = request.POST.get("name")
+    name = request.POST.get("name")
 
     error = None
     if not name:
@@ -54,7 +54,7 @@ def delete_testgroup(request):
     if request.method != "POST":
         return HttpResponse("wrong request method.")
 
-    name = name = request.POST.get("name")
+    name = request.POST.get("name")
 
     try:
         TestGroup.objects.get(name=name).delete()
@@ -73,6 +73,39 @@ def get_testgroup_detail(request, name: str):
 
     context = {"testgroup": testgroup}
     return render(request, "testgroup_detail.html", context)
+
+
+def rename_testgroup(request, name: str):
+    if request.method != "POST":
+        return HttpResponse("bad request method.")
+
+    testgroup: TestGroup
+    try:
+        testgroup = TestGroup.objects.get(name=name)
+    except TestGroup.DoesNotExist:
+        raise Http404(" This Test Group does not exist")
+
+    new_name = request.POST.get("name")
+
+    context = {}
+    error = None
+    if not new_name:
+        error = "name cannot be empty"
+        context["error"] = error
+    elif not re.fullmatch(testgroup_name_pattern, new_name):
+        error = "name can only contain letters, numbers, underscores, dashes and dots."
+        context["error"] = error
+    else:
+        try:
+            testgroup.name = new_name
+            testgroup.save()
+            name = new_name
+        except IntegrityError:
+            error = "a group with that name already exists."
+            context["error"] = error
+
+    context["name"] = name
+    return render(request, "testcases_detail_title.html", context)
 
 
 def list_testcases(request, name: str):
