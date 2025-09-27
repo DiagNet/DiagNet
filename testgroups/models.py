542fff1be672cb6fe38584612a7bd32d38ef2198
Benedikt <benedikt.theuretzbachner@gmail.com>
2025-09-27 17:50:46 +0200

    DN-27 feat/testgroups: add page for testgroup detail

--- testgroups/models.py
+++ testgroups/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from networktests.models import TestCase
 
 
 class TestGroup(models.Model):
     name = models.CharField(max_length=50, unique=True)
+
+    testcases = models.ManyToManyField(TestCase)
