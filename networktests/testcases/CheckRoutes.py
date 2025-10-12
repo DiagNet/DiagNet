3063d0182808e1fa0097851ae1a0c12606f20768
DrPacar <lukapacar11@gmail.com>
2025-10-12 22:55:48 +0200

    DN-33 create-test: add some useful testing classes

--- networktests/testcases/CheckRoutes.py
+++ networktests/testcases/CheckRoutes.py
@@ -3,6 +3,93 @@ from networktests.testcases.base import DiagNetTest
 
 class CheckRoutes(DiagNetTest):
     _required_params = [
+        {
+            "name": "type",
+            "type": "choice",
+            "choices": ["IPv4", "IPv6"],
+            "default_choice": "IPv4",
+            "description": "Selects if IPv4 or IPv6 Routes are checked"
+        },
+        {
+            "name": "target",
+            "type": "device"
+        },
+        {
+            "name": "routes",
+            "type": "list",
+            "description": "The routes to check",
+            "required": [
+                {
+                    "name": "destination_prefix",
+                    "type": "CIDR-value(type)",
+                    "description": "The destination network in CIDR notation"
+                },
+                {
+                    "name": "route_source",
+                    "type": "choice",
+                    "choices": [
+                        "ANY","CONNECTED (C)"
+                    ],
+                    "default_choice": "ANY",
+                    "description": "The origin or protocol through which a route was learned"
+                }
+            ],
+            "optional": [],
+            "mutually_exclusive": [("destination_prefix", "route_source")],
+            "constraints": ["min_length=1", "unique=true"]
+        }
+    ]
+
+    _required_params4 = [
+        "type:choices[IPv4,IPv6]{description=Selects if IPv4 or IPv6 Routes are checked}",
+        "target:device",
+        [ # List
+            """routes:list{description=The routes to check}""",
+            [ # Required Parameters
+                """destination_prefix:CIDR-value(type){description="The destination network in CIDR notation"}""",
+                [ # Choice
+                    "route_source:choices{description='The origin or protocol through which a route was learned'}",
+                    [
+                        "ANY",
+                        "CONNECTED (C)",
+                        "LOCAL (L)",
+                        "STATIC (S)",
+                        "STATIC* (S*)",
+                        "RIP (R)",
+                        "OSPF",
+                        "OSPF IA",
+                        "OSPF E1",
+                        "OSPF E2",
+                        "OSPF N1",
+                        "OSPF N2",
+                        "EIGRP (D)",
+                        "EIGRP EX (D EX)",
+                        "BGP (B)",
+                        "BGP IGP (B)",
+                        "BGP EGP (B)",
+                        "IS-IS (i)",
+                        "IS-IS L1 (i L1)",
+                        "IS-IS L2 (i L2)",
+                        "IS-IS L1/L2 (i L1/L2)",
+                        "IS-IS * (i *)"
+                    ],
+                    "ANY" # Default Selection
+                ]
+            ],
+            [ # Optional Parameters
+                "local_preference:number[min=0, max=1000000]{description='Local Preference of the BGP Route', required_if={route_source=r'BGP.*'}}",
+                "message_for_non_ospf:str{forbidden_if={route_source=r'OSPF.*'}}"
+            ],
+            [ # Mutually Exclusive Bindings
+
+            ],
+            [ # Constraints
+                "min_length=1",
+                "unique=true"
+            ]
+        ]
+    ]
+    _required_params2 = [
         """
         type:choices["IPv4","IPv6"]{description="Selects if IPv4 or IPv6 Routes are checked"},
         target:device,
