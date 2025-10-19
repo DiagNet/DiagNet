118e9be9e3347b3aa0a5932d96d495bc08ba3652
DrPacar <lukapacar11@gmail.com>
2025-10-19 18:42:28 +0200

    DN-33 create-test: implement Parameter Info View

--- static/js/create_test/handle_parameters/parameter_info_tab.js
+++ static/js/create_test/handle_parameters/parameter_info_tab.js
@@ -1,35 +1,105 @@
+// Main Container
 const parameterInfoContainer = document.getElementById('parameterInfoContainer');
 
+// Templates
+const listParameterInfo = document.getElementById('ListParameterInfo');
+const defaultParameterInfo = document.getElementById('RegularParameterInfo');
+const listItemSmallInfo = document.getElementById('ListItemSmallInfo');
+
+// Templates - List Item Info Page
+const listItemInfo = document.getElementById('ListItemInfo');
+const listItemInfoDefaultItem = document.getElementById('ListItemInfoDefaultItem');
+const listItemListing = document.getElementById('ListItemListing');
+
+// Fields
 const parentName = document.getElementById("infoTabParentName");
-const parentDescription = document.getElementById("infoTabParentDescription");
+const parentType = document.getElementById("infoTabParentType");
 
 const datatypeName = document.getElementById("infoTabDatatypeName");
-
-const inputDescription = document.getElementById("infoTabInputLabel");
-const inputContainer = document.getElementById("infoTabInputContainer");
-
+const inputDescription = document.getElementById("infoTabDatatypeDescription");
 
 /**
  * Displays Info about the current Parameter in the info tab.
  * @param {ParameterField} parameter
  */
 function displayParameterInfo(parameter) {
+    parameterInfoContainer.classList.remove('hidden');
+    parameterInfoContainer.innerHTML = "";
     switch (parameter.parameter['type']) {
         case 'list':
             displayListParameterInfo(parameter);
             break;
         default:
-            displayParameterInfo(parameter);
+            displayDefaultParameterInfo(parameter);
     }
 }
 
 
 /**
  * Displays Info about a given List Parameter.
- * @param {ParameterField} parameter The Parameter
+ * @param {ListField} parameter The Parameter
  */
 function displayListParameterInfo(parameter) {
+    const container = listParameterInfo.content.cloneNode(true).querySelector('div');
 
+    displayParentInfo(container, parameter);
+
+    container.querySelector('#infoTabDatatypeName').textContent = parameter.parameter['type'];
+    container.querySelector('#infoTabDatatypeDescription').textContent = "Work in Progress";
+
+    const currentItems = parameter.getValue();
+    if (currentItems.length === 0) {
+
+    } else {
+        for (const item of currentItems) {
+            const listItemViewContainer = listItemSmallInfo.content.cloneNode(true).querySelector('div');
+
+            const paramName = Object.keys(item)[0];
+            const paramValue = item[paramName];
+
+
+            listItemViewContainer.querySelector('#infoText').textContent = `${paramName} = ${paramValue}`;
+            listItemViewContainer.querySelector('#getInfoForItem').addEventListener("click", () => {
+                parameterInfoContainer.classList.remove('hidden');
+                parameterInfoContainer.innerHTML = "";
+                displayListItemInfo([item], parameterInfoContainer);
+            });
+            listItemViewContainer.querySelector('#deleteItem').addEventListener("click", () => {
+                parameter.removeValue(item);
+                listItemViewContainer.remove();
+            });
+            container.appendChild(listItemViewContainer);
+        }
+    }
+
+
+    parameterInfoContainer.appendChild(container);
+}
+
+
+function displayListItemInfo(item, containerToAddTo) {
+    for (const collection of item) {
+        for (const [key, value] of Object.entries(collection)) {
+            if (Array.isArray(value)) {
+                const templateContainer = listItemListing.content.cloneNode(true).querySelector('div');
+                const itemContainer = templateContainer.querySelector("#listItemContainer")
+                templateContainer.querySelector("#listTitle").textContent = key;
+                // List View
+                displayListItemInfo(value, itemContainer);
+                containerToAddTo.appendChild(templateContainer);
+            } else {
+                // Default View
+                containerToAddTo.appendChild(displayItemInfoDefaultParameter(key, value));
+            }
+        }
+    }
+}
+
+function displayItemInfoDefaultParameter(name, value) {
+    const container = listItemInfoDefaultItem.content.cloneNode(true).querySelector('div');
+    container.querySelector("#name").textContent = name;
+    container.querySelector("#value").textContent = value;
+    return container;
 }
 
 /**
@@ -37,5 +107,29 @@ function displayListParameterInfo(parameter) {
  * @param {ParameterField} parameter The Parameter
  */
 function displayDefaultParameterInfo(parameter) {
+    const container = defaultParameterInfo.content.cloneNode(true).querySelector('div');
 
+    displayParentInfo(container, parameter);
+
+    container.querySelector('#infoTabDatatypeName').textContent = parameter.parameter['type'];
+    container.querySelector('#infoTabDatatypeDescription').textContent = "Work in Progress";
+
+
+    parameterInfoContainer.appendChild(container);
+}
+
+
+function displayParentInfo(container, parameter) {
+    let parentName = parameter.parameter['parent_name'];
+    let parentType;
+    if (!parentName) {
+        parentName = previousTestClass;
+        parentType = "TestCase";
+    } else {
+        parentName = parentName.parameter['name'];
+        parentType = parameter.parameter['parent_type'];
+    }
+
+    container.querySelector('#infoTabParentName').textContent = parentName;
+    container.querySelector('#infoTabParentType').textContent = parentType;
 }
