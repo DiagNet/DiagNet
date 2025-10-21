db240836e916d4150d7e8fe85d95d05a781e9209
Karun Sandhu <129101708+MrSom3body@users.noreply.github.com>
2025-10-21 16:02:06 +0200

    devices/templates: split up index page and list page

--- devices/urls.py
+++ devices/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.DeviceListView.as_view(), name="device-list"),
+    path("", views.index, name="devices-page"),
+    path("api/get-table", views.DeviceListView.as_view(), name="devices-table"),
     path("create/", views.DeviceCreate.as_view(), name="device-create"),
     path("all/", views.get_all_devices, name="get-all-devices"),
     path("<str:pk>/", views.DeviceUpdate.as_view(), name="device-detail"),
     path("<str:pk>/delete", views.DeviceDelete.as_view(), name="device-delete"),
     path("<str:pk>/check", views.device_check, name="device-check"),
+    path("api/", views.DeviceListView.as_view(), name="device-table"),
 ]
