362007d8f29a72b011a8f64762d2fd9af184c098
DrPacar <lukapacar11@gmail.com>
2025-10-25 20:41:05 +0200

    DN-33 create-test: add optional labels for parameters in lists, fix overflowing behaviour, fixed some bugs regarding submission

---
+++ networktests/testcases/CheckRoutingTable.py
@@ -0,0 +1,83 @@
+from networktests.testcases.base import DiagNetTest
+
+
+class CheckRoutingTable(DiagNetTest):
+    _required_params = [
+        {
+            "name": "target_device",
+            "display_name": "Target Device",
+            "type": "device",
+            "description": "The device from which to test the routes"
+        },
+        {
+            "name": "address_family",
+            "display_name": "Address Family",
+            "type": "choice",
+            "choices": ["IPv4", "IPv6"],
+            "default_choice": "IPv4",
+            "description": "The address family associated with the routes"
+        },
+        {
+            "name": "routes",
+            "display_name": "Routes",
+            "type": "list",
+            "required": [
+                {
+                    "name": "route_origin",
+                    "display_name": "Route Origin",
+                    "type": "choice",
+                    "choices": [
+                        "CONNECTED (C)",
+                        "LOCAL (L)",
+                        "STATIC (S)",
+                        "STATIC* (S*)",
+                        "RIP (R)",
+                        "OSPF (O)",
+                        "OSPF IA (O IA)",
+                        "OSPF E1 (O E1)",
+                        "OSPF E2 (O E2)",
+                        "EIGRP (D)",
+                        "EIGRP EX (D EX)",
+                        "BGP (B)",
+                        "IS-IS (i)",
+                        "IS-IS L1 (i L1)",
+                        "IS-IS L2 (i L2)",
+                        "IS-IS L1/L2 (i L1/L2)"
+                    ],
+                    "default_choice": "ANY",
+                    "description": "Decides which routing protocol the route is expecting",
+                },
+                {
+                    "name": "prefix",
+                    "display_name": "Prefix - Network Destination",
+                    "type": "CIDR-value(address_family)",
+                    "description": "Destination network in CIDR notation",
+                }
+            ],
+            "optional": [
+                {
+                    "name": "administrative distance",
+                    "display_name": "Administrative Distance",
+                    "type": "device",
+                    "description": "[AD (trust level)/metric (cost to reach)]",
+                },
+                {
+                    "name": "next_hop",
+                    "display_name": "Next Hop",
+                    "type": "value(address_family)",
+                    "description": "Next device used to reach the network",
+                },
+                {
+                    "name": "outgoing_interface",
+                    "display_name": "Outgoing Interface",
+                    "type": "Interface",
+                    "description": "Interface used to reach the next hop",
+                },
+            ],
+            "mutually_exclusive": [],
+            "constraints": []
+        },
+    ]
+
+    def test_fetch_routes(self):
+        return True
