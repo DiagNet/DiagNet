01884f9ebf66a56419ae30c9f8d0bbceb51a5ae7
Karun Sandhu <karun@sndh.dev>
2025-10-31 22:47:34 +0100

    devices/urls: put everything that shouldn't be user accessed behind api/

--- devices/urls.py
+++ devices/urls.py
@@ -4,9 +4,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="devices-page"),
     path("api/get-table", views.DeviceListView.as_view(), name="devices-table"),
-    path("create/", views.DeviceCreate.as_view(), name="device-create"),
-    path("all/", views.get_all_devices, name="get-all-devices"),
-    path("<str:pk>/", views.DeviceUpdate.as_view(), name="device-edit"),
-    path("<str:pk>/delete", views.DeviceDelete.as_view(), name="device-delete"),
-    path("<str:pk>/check", views.device_check, name="device-check"),
+    path("api/create/", views.DeviceCreate.as_view(), name="device-create"),
+    path("api/update/<str:pk>", views.DeviceUpdate.as_view(), name="device-edit"),
+    path("api/delete/<str:pk>", views.DeviceDelete.as_view(), name="device-delete"),
+    path("api/check/<str:pk>", views.device_check, name="device-check"),
 ]
