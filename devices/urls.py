bd26b77c7d701d501ca6ac04b184ebd2488662c9
Karun Sandhu <karun@sndh.dev>
2025-11-11 14:46:30 +0100

    devices/urls: add trailing slashes

--- devices/urls.py
+++ devices/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="devices-page"),
-    path("api/get-table", views.DeviceListView.as_view(), name="devices-table"),
+    path("api/get-table/", views.DeviceListView.as_view(), name="devices-table"),
     path("api/create/", views.DeviceCreate.as_view(), name="device-create"),
-    path("api/update/<str:pk>", views.DeviceUpdate.as_view(), name="device-update"),
-    path("api/delete/<str:pk>", views.DeviceDelete.as_view(), name="device-delete"),
-    path("api/check/<str:pk>", views.device_check, name="device-check"),
+    path("api/update/<str:pk>/", views.DeviceUpdate.as_view(), name="device-update"),
+    path("api/delete/<str:pk>/", views.DeviceDelete.as_view(), name="device-delete"),
+    path("api/check/<str:pk>/", views.device_check, name="device-check"),
 ]
