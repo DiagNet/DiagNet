e96c7de393a839f3496d8e29f761e4cea31fd457
Karun Sandhu <karun@sndh.dev>
2025-11-26 15:21:01 +0100

    accounts/signals: check for secure password

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,3 +1,4 @@
+import re
 from getpass import getpass
 
 from django.contrib.auth import get_user_model
@@ -10,6 +11,11 @@ def create_superuser_if_none(sender, **kwargs):
     User = get_user_model()
     if not User.objects.filter(is_superuser=True).exists():
         print("⚠️ No superuser found. Creating one...")
+
+        password_pattern = (
+            r"^(?!.*\s)(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W)[A-Za-z\d\W]{6,20}$"
+        )
+
         username = input("Enter admin username: ")
 
         while True:
@@ -17,6 +23,11 @@ def create_superuser_if_none(sender, **kwargs):
             password_verify = getpass("Verify admin password: ")
             if password == password_verify:
                 break
+            elif not re.match(password_pattern, password):
+                print(
+                    "❌ Password must be 6-20 characters long, contain at least one lowercase letter, "
+                    "one uppercase letter, one digit, one special character, and have no spaces. Please try again."
+                )
             else:
                 print("❌ Passwords do not match. Please try again.")
 
