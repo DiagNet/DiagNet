8fe75107c0e99a9dabb2f7870c03433166c4e549
Karun Sandhu <karun@sndh.dev>
2025-11-29 18:49:27 +0100

    devices: port everything from name as pk to a seperate id field

--- devices/urls.py
+++ devices/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     path("api/get-devices/", views.get_all_devices, name="devices-all"),
     path("api/get-table/", views.DeviceListView.as_view(), name="devices-table"),
     path("api/create/", views.DeviceCreate.as_view(), name="device-create"),
-    path("api/update/<str:pk>/", views.DeviceUpdate.as_view(), name="device-update"),
-    path("api/delete/<str:pk>/", views.DeviceDelete.as_view(), name="device-delete"),
-    path("api/check/<str:pk>/", views.device_check, name="device-check"),
+    path("api/update/<int:pk>/", views.DeviceUpdate.as_view(), name="device-update"),
+    path("api/delete/<int:pk>/", views.DeviceDelete.as_view(), name="device-delete"),
+    path("api/check/<int:pk>/", views.device_check, name="device-check"),
     path("api/export/", views.export_devices_from_yaml, name="devices-export"),
     path("import-devices/", views.import_devices_from_yaml, name="device-import"),
 ]
