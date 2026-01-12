34d79937556aa3682832f0b61d75a58d3044ffac
benedikt <benedikt.theuretzbachner@gmail.com>
2026-01-12 21:07:26 +0100

    devices: migrations

--- devices/migrations/0006_device_id_alter_device_name.py
+++ devices/migrations/0006_device_id_alter_device_name.py
@@ -4,20 +4,23 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('devices', '0005_remove_device_status'),
+        ("devices", "0005_remove_device_status"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='device',
-            name='id',
-            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
+            model_name="device",
+            name="id",
+            field=models.BigAutoField(
+                auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+            ),
         ),
         migrations.AlterField(
-            model_name='device',
-            name='name',
-            field=models.CharField(max_length=100, unique=True, verbose_name='Hostname'),
+            model_name="device",
+            name="name",
+            field=models.CharField(
+                max_length=100, unique=True, verbose_name="Hostname"
+            ),
         ),
     ]
