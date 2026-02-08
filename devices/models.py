f6f682c55a88018d2e57bbd0dc161e9341bfd551
Karun Sandhu <karun@sndh.dev>
2026-02-08 15:43:51 +0100

    devices/models: increase size to 512 but limit in forms to 256

--- devices/models.py
+++ devices/models.py
@@ -51,9 +51,9 @@ class Device(models.Model):
         choices=DEVICE_TYPES,
     )
     username = models.CharField(max_length=100)
-    password = models.CharField(max_length=255)
+    password = models.CharField(max_length=512)
 
-    enable_password = models.CharField(max_length=255)
+    enable_password = models.CharField(max_length=512)
 
     class Meta:
         constraints = [
