5a189e9ed2559791ef5a13a6cc2f94ffb5e54831
Karun Sandhu <karun@sndh.dev>
2026-02-08 15:28:55 +0100

    devices: show a warning when editing if the device password cannot be decrypted

--- devices/views.py
+++ devices/views.py
@@ -68,6 +68,11 @@ class DeviceUpdate(UpdateView):
     form_class = DeviceForm
     template_name = "devices/partials/device_form.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["encryption_error"] = not self.object.has_valid_encryption
+        return context
+
     def get_success_url(self):
         return reverse_lazy("device-update", kwargs={"pk": self.object.pk})
 
