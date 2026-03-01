c468f8b4203bd2c931dd3f96fb74256d1814869f
Karun Sandhu <karun@sndh.dev>
2026-03-01 15:58:31 +0100

    diagnet: remove Django admin

--- diagnet/settings.py
+++ diagnet/settings.py
@@ -119,7 +119,6 @@ ALLOWED_HOSTS = [
 
 INSTALLED_APPS = [
     "whitenoise.runserver_nostatic",  # WARN don't move it down
-    "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
