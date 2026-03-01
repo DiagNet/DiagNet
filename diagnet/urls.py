0d5edaaf8388809a3ae64414b04f169534987175
Karun Sandhu <karun@sndh.dev>
2026-03-01 15:58:31 +0100

    diagnet: remove Django admin

--- diagnet/urls.py
+++ diagnet/urls.py
@@ -17,11 +17,9 @@ Including another URLconf
 
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("auth/", include("django.contrib.auth.urls")),
     path("", include("dashboard.urls")),
     path("accounts/", include("accounts.urls")),
